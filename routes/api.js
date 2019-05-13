2ba52bbb153cb27772222647c5465a373eb24a49
Glitch (woozy-cornet) <none>
2019-05-13 12:17:45 +0000

    Good Progress, now trying to get a getStockData function to return

--- routes/api.js
+++ routes/api.js
@@ -16,10 +16,18 @@ const MONGODB_CONNECTION_STRING = process.env.DB;
 
 const alphaVantageAPIKEY = process.env.APIKEY;
 
-function getStock(stockSymbol) {
-  let url = 'https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${alphaVantageAPIKEY}';
-  fetch(url).then(jsonData => {
-    console.log(jsonData)
+function getStockData(stockSymbol) {
+  let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${alphaVantageAPIKEY}`;
+  console.log(url);
+  fetch(url)
+    .then(response => response.json())
+    .then(jsonData => {
+      let response = {
+        'stock': jsonData['Global Quote']["01. symbol"], 
+        'price': jsonData['Global Quote']['05. price']
+      }
+      console.log(response);
+      return response;
   }); 
 }
 
@@ -27,15 +35,16 @@ module.exports = function (app) {
   
   app.route('/api/stock-prices')
     .get(function (req, res){
-      var response = {}
+      var response = {stockData: {}}
       var stock = req.query.stock;
       var like = req.query.like || false;
       var reqIP = req.connection.remoteAddress;
+      console.log('getStockData return: ' + getStockData(req.query.stock));
       response.stockData.stock = req.query.stock.toUpperCase(); 
       response.stockData.price = 1;
       response.stockData.likes = 1;
-      console.log(response);
-      res.json({}); 
+      // console.log(response);
+      res.json(response); 
       // res.json({stockData:{stock:"GOOG",price:"786.90",likes:1}});
   });
     
