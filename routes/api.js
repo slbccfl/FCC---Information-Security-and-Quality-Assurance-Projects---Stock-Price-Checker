6a26d307b17b1bd1b0785121923d1300648052ae
Glitch (woozy-cornet) <none>
2019-05-15 16:56:42 +0000

    Did that just work?

--- routes/api.js
+++ routes/api.js
@@ -11,24 +11,13 @@
 var expect = require('chai').expect;
 const MongoClient = require('mongodb');
 const fetch       = require("node-fetch");
+const getStockData = require('../middleware/getStockData');
 
 const MONGODB_CONNECTION_STRING = process.env.DB;
 
 const alphaVantageAPIKEY = process.env.APIKEY;
 
-const getStockData = (req, res, next) => {
-  let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${req.query.stock}&apikey=${alphaVantageAPIKEY}`;
-  console.log(url);
-  fetch(url)
-    .then(response => response.json())
-    .then(jsonData => {
-      res.locals.stockData = {
-        'stock': jsonData['Global Quote']["01. symbol"], 
-        'price': jsonData['Global Quote']['05. price']
-      }
-  }); 
-  next();
-}
+
 
 module.exports = function (app) {
   app.route('/api/stock-prices')
@@ -37,8 +26,7 @@ module.exports = function (app) {
       var stock = req.query.stock;
       var like = req.query.like || false;
       var reqIP = req.connection.remoteAddress;
-      app.use(getStockData(req, res, next) 
-      console.log('getStockData return: ' + getStockData(req.query.stock));
+      app.use(getStockData)
       response.stockData.stock = req.query.stock.toUpperCase(); 
       response.stockData.price = 1;
       response.stockData.likes = 1;
