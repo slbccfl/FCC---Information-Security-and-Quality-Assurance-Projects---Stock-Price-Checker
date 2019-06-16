fbcdff27db16c8fc732c7a71cf1f27022be22e04
Glitch (woozy-cornet) <none>
2019-06-16 14:06:35 +0000

    Tried to make test setup and teardown a test suite

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -15,22 +15,30 @@ var os = require('os');
 const MongoClient = require('mongodb');
 const MONGODB_CONNECTION_STRING = process.env.DB;
 
+const ip = '::ffff:' + os.networkInterfaces().lo[0].address;
+
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  var ip = '::ffff:' + os.networkInterfaces().lo[0].address;
-  MongoClient.connect(MONGODB_CONNECTION_STRING, async (err, db) => {
-    if (err) throw err
-    const collection = db.collection("stocks");
-    // set two boogus likes for GOOG. 
-    let updateDoc = {stockSym: 'GOOG', IP: 0} 
-    collection.update(updateDoc, updateDoc, {upsert: true})
-    updateDoc = {stockSym: 'GOOG', IP: 1} 
-    collection.update(updateDoc, updateDoc, {upsert: true})
-    // remove like for current ip
-    let removeDoc = {stockSym: 'GOOG', IP: ip} 
-    collection.remove(removeDoc); 
-  });
+  suite('Setup for Functional Tests', function() {
+    try { 
+      MongoClient.connect(MONGODB_CONNECTION_STRING, async (err, db) => {
+        const collection = db.collection("stocks");
+        // set two boogus likes for GOOG. 
+        let updateDoc = {stockSym: 'GOOG', IP: 0} 
+        collection.update(updateDoc, updateDoc, {upsert: true})
+        updateDoc = {stockSym: 'GOOG', IP: 1} 
+        collection.update(updateDoc, updateDoc, {upsert: true})
+        // remove like for current ip
+        let removeDoc = {stockSym: 'GOOG', IP: ip} 
+        collection.remove(removeDoc); 
+      });
+    } catch (err) { 
+      throw err
+    }
+    
+  })
+
   suite('GET /api/stock-prices => stockData object', function() {
     let likeCount = 0;
     test('1 stock', function(done) {
@@ -49,22 +57,23 @@ suite('Functional Tests', function() {
         done(); 
       });
     });
+  });
 
-    test('1 stock with like', function(done) {
-    chai.request(server)
-      .get('/api/stock-prices')
-      .query({stock: 'GOOG', like:true}) 
-      .end(function(err, res){
-        // console.log('body: ' + JSON.stringify(res.body));
-        assert.equal(res.status, 200);
-        assert.property(res.body.stockData, 'stock');
-        assert.property(res.body.stockData, 'price');
-        assert.property(res.body.stockData, 'likes');
-        assert.equal(res.body.stockData.stock, 'GOOG');
-        assert.equal(res.body.stockData.likes, likeCount+1);
-        done(); 
-      });
-    });
+//     test('1 stock with like', function(done) {
+//     chai.request(server)
+//       .get('/api/stock-prices')
+//       .query({stock: 'GOOG', like:true}) 
+//       .end(function(err, res){
+//         // console.log('body: ' + JSON.stringify(res.body));
+//         assert.equal(res.status, 200);
+//         assert.property(res.body.stockData, 'stock');
+//         assert.property(res.body.stockData, 'price');
+//         assert.property(res.body.stockData, 'likes');
+//         assert.equal(res.body.stockData.stock, 'GOOG');
+//         assert.equal(res.body.stockData.likes, likeCount+1);
+//         done(); 
+//       });
+//     });
       
 //       test('1 stock with like again (ensure likes arent double counted)', function(done) {
 //           done();
@@ -77,19 +86,23 @@ suite('Functional Tests', function() {
 //       test('2 stocks with like', function(done) {
 //           done();
 //       });
-
-  });
-  MongoClient.connect(MONGODB_CONNECTION_STRING, async (err, db) => {
-    if (err) throw err
-    const collection = db.collection("stocks");
-    // remove test likes for GOOG. 
-    let removeDoc = {stockSym: 'GOOG', IP: 0} 
-    collection.remove(removeDoc); 
-    removeDoc = {stockSym: 'GOOG', IP: 1} 
-    collection.remove(removeDoc); 
-    removeDoc = {stockSym: 'GOOG', IP: ip} 
-    console.log(removeDoc)
-    collection.remove(removeDoc); 
+  
+  suite('Teardown after Functional Tests', function() {
+    try { 
+      MongoClient.connect(MONGODB_CONNECTION_STRING, async (err, db) => {
+        if (err) throw err
+        const collection = db.collection("stocks");
+        // remove test likes for GOOG. 
+        let removeDoc = {stockSym: 'GOOG', IP: 0} 
+        collection.remove(removeDoc); 
+        removeDoc = {stockSym: 'GOOG', IP: 1} 
+        collection.remove(removeDoc); 
+        removeDoc = {stockSym: 'GOOG', IP: ip} 
+        collection.remove(removeDoc); 
+      });
+    } catch (err) { 
+      throw err
+    }
   });
 
 });
